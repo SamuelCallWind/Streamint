1327d145194feeb296a5a7549e255c9449bb2589
Samuel Callu <Samcallwind@gmail.com>
2025-10-06 13:31:30 +0200

    Added the base for launching the app

--- src/main/java/com/streamint/StreamintApplication.java
+++ src/main/java/com/streamint/StreamintApplication.java
@@ -1,13 +1,26 @@
 package com.streamint;
 
-import org.springframework.boot.SpringApplication;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StreamintApplication {
+public class StreamintApplication extends Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StreamintApplication.class, args);
-	}
+    @Override
+    public void start(Stage stage) {
+        Button button = new Button("Click me!");
+        StackPane root = new StackPane(button);
+        Scene scene = new Scene(root, 500, 300);
+        stage.setScene(scene);
+        stage.setTitle("Hello world!");
+        stage.show();
+    }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
